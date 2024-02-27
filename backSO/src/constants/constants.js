97eb9498c33ce307d36570b3137f0b474a9ee691
Alvaro Gamez Chavez <gaositd@gmail.com>
2024-02-27 10:22:26 -0600

    newServer start, failed

--- backSO/src/constants/constants.js
+++ backSO/src/constants/constants.js
@@ -5,10 +5,13 @@ const LOGS = `sudo docker logs --tail 5 mysql55`;
 const TAIL = 'logs --tail';
 const CAD_VACIA = '        ';
 
+const SERVER_PATH ='../../../servers/'
+
 module.exports = {
   ERRORDATA,
   CONTAINERSNAMES,
   LOGS,
   TAIL,
   CAD_VACIA,
+  SERVER_PATH,
 };
--- backSO/src/routes/controller/newServer.js
+++ backSO/src/routes/controller/newServer.js
@@ -1,4 +1,63 @@
-const postNewServer = () => {return {msg:"null"}};
+const fs = require('fs');
+const { randomUUID } = require('crypto');
+const {
+  ERRORDATA,
+  SERVER_PATH,
+} = require('../../constants/constants');
+ const servers = require('../../../../servers/Serverx.json');
+
+let id;
+let data = [];
+
+const postNewServer = async (res, req) => {
+  const fileName = 'SERVERS.json';
+  const {
+    server, // IP
+    enviroment,
+    disable,
+  } = req.body;
+  
+  if (server.length === 0 || enviroment.length === 0 ) {
+    console.log(ERRORDATA);
+    return res.status(500).json({ msg: ERRORDATA });
+  }
+
+  if(disable === null){
+    disable = true;
+  }
+
+  data = [
+    {
+      id:`${randomUUID()}`,
+      server:`${server}`,
+      enviroment:`${enviroment}`,
+      disable:`${disable}`
+    }
+  ];
+
+  try {
+    // await fs.appendFile(`${servers}/${fileName}`, data, (err) => {
+    //   if(err){
+    //     console.log(err);
+    //     return res
+    //     .status(500)
+    //     .json({msg:error});
+    //   }
+    // });
+
+    await fs.appendFile(`${servers}/${fileName}`, data, {flag: w});
+    return res.json({
+      data,
+      code:200,
+    });
+  } catch (error) {
+    console.log(error);
+    return res
+      .status(500)
+      .json({msg:error});
+  }
+  
+};
 
 module.exports = {
   postNewServer,
